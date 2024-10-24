d5960808d1896d5f3d27b30fd865a090ee00478a
eliifgokdemiir <gokdemirelif12@gmail.com>
2024-10-24 10:29:45 +0300

    Initial Commit

---
+++ ProjectLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Elif Gökdemir on 24.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
